a6f0a7d3077d9b8816ca7c3c966d962a892423a5
Bryan Hoffman <bryanhoffman87@gmail.com>
2022-04-10 11:51:35 -0400

    typescript based

---
+++ globals.ts
@@ -0,0 +1,5 @@
+var instruments = {};
+const context = new AudioContext();
+var tempo = 140.0;
+var numMeasures = 2;
+var numPads = 4 * numMeasures; // 4/4 time
---
+++ public/globals.js
@@ -0,0 +1,6 @@
+var instruments = {};
+const context = new AudioContext();
+var tempo = 140.0;
+var numMeasures = 2;
+var numPads = 4 * numMeasures; // 4/4 time
+//# sourceMappingURL=globals.js.map
